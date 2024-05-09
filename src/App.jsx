459c5b2797d7130df1f9a3b677895a39209c8ee4
lasithadilshan <37534587+lasithadilshan@users.noreply.github.com>
2024-05-09 07:44:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css';
+import React from 'react';
+import Dashboard from './Dashboard';
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Dashboard.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div class="container relative z-40 mx-auto mt-12">
+      <div class="flex flex-wrap justify-center mx-auto lg:w-full md:w-5/6 xl:shadow-small-blue">
+        <a href="#" class="block w-1/2 py-10 text-center border lg:w-1/4">
+          <div>
+            <img
+              src="https://redpixelthemes.com/assets/images/icon-portfolio-green.svg"
+              class="block mx-auto"
+            />
+
+            <p class="pt-4 text-sm font-medium capitalize font-body text-green-900 lg:text-lg md:text-base md:pt-6">
+              portfolio
+            </p>
+          </div>
+        </a>
+
+        <a href="#" class="block w-1/2 py-10 text-center border lg:w-1/4">
+          <div>
+            <img
+              src="https://redpixelthemes.com/assets/images/icon-blog-green.svg"
+              class="block mx-auto"
+            />
+
+            <p class="pt-4 text-sm font-medium capitalize font-body text-green-900 lg:text-lg md:text-base md:pt-6">
+              blog
+            </p>
+          </div>
+        </a>
+
+        <a href="#" class="block w-1/2 py-10 text-center border lg:w-1/4">
+          <div>
+            <img
+              src="https://redpixelthemes.com/assets/images/icon-ecommerce-green.svg"
+              class="block mx-auto"
+            />
+
+            <p class="pt-4 text-sm font-medium capitalize font-body text-green-900 lg:text-lg md:text-base md:pt-6">
+              ecommerce
+            </p>
+          </div>
+        </a>
+
+        <a href="#" class="block w-1/2 py-10 text-center border lg:w-1/4">
+          <div>
+            <img
+              src="https://redpixelthemes.com/assets/images/icon-startup-green.svg"
+              class="block mx-auto"
+            />
+
+            <p class="pt-4 text-sm font-medium capitalize font-body text-green-900 lg:text-lg md:text-base md:pt-6">
+              startup
+            </p>
+          </div>
+        </a>
+
+        <a href="#" class="block w-1/2 py-10 text-center border lg:w-1/4">
+          <div>
+            <img
+              src="https://redpixelthemes.com/assets/images/icon-business-green.svg"
+              class="block mx-auto"
+            />
+
+            <p class="pt-4 text-sm font-medium capitalize font-body text-green-900 lg:text-lg md:text-base md:pt-6">
+              business
+            </p>
+          </div>
+        </a>
+
+        <a href="#" class="block w-1/2 py-10 text-center border lg:w-1/4">
+          <div>
+            <img
+              src="https://redpixelthemes.com/assets/images/icon-lifestyle-green.svg"
+              class="block mx-auto"
+            />
+
+            <p class="pt-4 text-sm font-medium capitalize font-body text-green-900 lg:text-lg md:text-base md:pt-6">
+              lifestyle
+            </p>
+          </div>
+        </a>
+
+        <a href="#" class="block w-1/2 py-10 text-center border lg:w-1/4">
+          <div>
+            <img
+              src="https://redpixelthemes.com/assets/images/icon-landing-page-green.svg"
+              class="block mx-auto"
+            />
+
+            <p class="pt-4 text-sm font-medium capitalize font-body text-green-900 lg:text-lg md:text-base md:pt-6">
+              landing pages
+            </p>
+          </div>
+        </a>
+
+        <a href="#" class="block w-1/2 py-10 text-center border lg:w-1/4">
+          <div>
+            <img
+              src="https://redpixelthemes.com/assets/images/icon-health-green.svg"
+              class="block mx-auto"
+            />
+
+            <p class="pt-4 text-sm font-medium capitalize font-body text-green-900 lg:text-lg md:text-base md:pt-6">
+              health
+            </p>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
